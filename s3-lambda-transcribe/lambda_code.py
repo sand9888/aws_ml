7421411fa7a7a48a892890cccb07f65d0f55d088
Andrew <sand9888@gmail.com>
2020-02-01 14:23:23 +0100

    Update lambda_code.py

--- s3-lambda-transcribe/lambda_code.py
+++ s3-lambda-transcribe/lambda_code.py
@@ -4,7 +4,7 @@ import json
 
 def lambda_handler(event, context):
 
-    print(json.dumps(event))
+    print(json.dumps(event)) # fire Cloudwatch here
 
     record = event['Records'][0]
     
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
     s3object = record['s3']['object']['key']
     
     s3Path = "s3://" + s3bucket + "/" + s3object
-    jobName = s3object + '-' + str(uuid.uuid4())
+    jobName = s3object + '-' + str(uuid.uuid4()) # job name must be unique, so add randomness
 
     client = boto3.client('transcribe')
 
